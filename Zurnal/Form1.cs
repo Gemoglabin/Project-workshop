abed535b1311dabfa3c8de960526123e21bbb63d
Игорь Щеглов <igorsheglov2001@gmail.com>
2020-03-09 16:06:14 +0200

    Добавил комментарий

--- Zurnal/Form1.cs
+++ Zurnal/Form1.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Zurnal
-{
+namespace Zurnal 
+{//Я ТЕСТИРУЮ 957
     public partial class Form1 : Form
     {
         public Form1()
