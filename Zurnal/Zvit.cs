3b1433073be86e887980a7b1efcfb296f382b5a4
Julia Shpytko <juliashpytko777@gmail.com>
2020-04-08 18:55:32 +0300

    формирование отчёта в ворд

--- Zurnal/Zvit.cs
+++ Zurnal/Zvit.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Office.Interop;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Zurnal
 {
@@ -52,35 +51,35 @@ namespace Zurnal
             InputGroup();
         }
 
-		private void розкладToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-		}
+        private void розкладToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
 
-		private void cmbgroup_SelectedIndexChanged(object sender, EventArgs e)
-		{
+        private void cmbgroup_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void DropDownList(object sender, EventArgs e)
-		{
+        private void DropDownList(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void cmbgroup_KeyPress(object sender, KeyPressEventArgs e)
-		{
-			e.Handled = true;
-		}
+        private void cmbgroup_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
 
-		private void label5_Click(object sender, EventArgs e)
-		{
-			this.Hide();
-			Form1 boun = new Form1();
-			boun.Show();
-		}
+        private void label5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 boun = new Form1();
+            boun.Show();
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
             if (radioButton1.Checked)
             {
                 string z = @"SELECT  s.fio, SUM(v.mark) FROM student s, visit v, timetable t WHERE v.id_stud=s.id_stud and v.id_time=t.id_time and t.name_gr = 'П-62' and t.date BETWEEN date('now','weekday 0', '-14 day') and date('now','weekday 0','-7 day') GROUP BY s.fio ORDER BY s.fio ASC;";
@@ -151,5 +150,116 @@ namespace Zurnal
 
             ExcelApp.Visible = true;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Filter = "Word Documents (*.docx)|*.docx";
+
+            sfd.FileName = "Звіт.docx";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Export_Data_To_Word(dataGridView1, sfd.FileName);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Export_Data_To_Word(DataGridView dataGridView1, string filename)
+        {
+            if (dataGridView1.Rows.Count != 0)
+            {
+                int RowCount = dataGridView1.Rows.Count;
+                int ColumnCount = dataGridView1.Columns.Count;
+                Object[,] DataArray = new object[RowCount + 1, ColumnCount + 1];
+
+                //Добавление строк
+                int r = 0;
+                for (int c = 0; c <= ColumnCount - 1; c++)
+                {
+                    for (r = 0; r <= RowCount - 1; r++)
+                    {
+                        DataArray[r, c] = dataGridView1.Rows[r].Cells[c].Value;
+                    } //Конец добавления строк
+                } //Конец добавления колонок
+
+                Word.Document oDoc = new Word.Document();
+                oDoc.Application.Visible = true;
+
+                //Ориентация таблицы
+                oDoc.PageSetup.Orientation = Word.WdOrientation.wdOrientPortrait;
+
+
+                dynamic oRange = oDoc.Content.Application.Selection.Range;
+                string oTemp = "";
+                for (r = 0; r <= RowCount - 1; r++)
+                {
+                    for (int c = 0; c <= ColumnCount - 1; c++)
+                    {
+                        oTemp = oTemp + DataArray[r, c] + "\t";
+
+                    }
+                }
+
+                //Формат таблицы
+                oRange.Text = oTemp;
+
+                object Separator = Word.WdTableFieldSeparator.wdSeparateByTabs;
+                object ApplyBorders = true;
+                object AutoFit = true;
+                object AutoFitBehavior = Word.WdAutoFitBehavior.wdAutoFitContent;
+
+                oRange.ConvertToTable(ref Separator, ref RowCount, ref ColumnCount,
+                                      Type.Missing, Type.Missing, ref ApplyBorders,
+                                      Type.Missing, Type.Missing, Type.Missing,
+                                      Type.Missing, Type.Missing, Type.Missing,
+                                      Type.Missing, ref AutoFit, ref AutoFitBehavior, Type.Missing);
+
+                oRange.Select();
+
+                oDoc.Application.Selection.Tables[1].Select();
+                oDoc.Application.Selection.Tables[1].Rows.AllowBreakAcrossPages = 0;
+                oDoc.Application.Selection.Tables[1].Rows.Alignment = 0;
+                oDoc.Application.Selection.Tables[1].Rows[1].Select();
+                oDoc.Application.Selection.InsertRowsAbove(1);
+                oDoc.Application.Selection.Tables[1].Rows[1].Select();
+
+                //Стиль названия колонок
+                oDoc.Application.Selection.Tables[1].Rows[1].Range.Bold = 1;
+                oDoc.Application.Selection.Tables[1].Rows[1].Range.Font.Name = "Times New Roman";
+                oDoc.Application.Selection.Tables[1].Rows[1].Range.Font.Size = 14;
+
+                
+                for (int c = 0; c <= ColumnCount - 1; c++)
+                {
+                    oDoc.Application.Selection.Tables[1].Cell(1, c + 1).Range.Text = dataGridView1.Columns[c].HeaderText;
+                }
+
+                //Стиль таблицы
+                oDoc.Application.Selection.Tables[1].set_Style("Сетка таблицы");
+                oDoc.Application.Selection.Tables[1].Rows[1].Select();
+                oDoc.Application.Selection.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+                
+
+
+                //Сохранение файла
+                oDoc.SaveAs2(filename);
+
+                
+            }
+        }
+
+
+
+
+        
+
     }
 }
